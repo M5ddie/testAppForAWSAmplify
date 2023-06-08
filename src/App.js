f1289381221f5dcf1eb264c63fff875aff13d7d4
M5ddie <m5ddieperez@gmail.com>
2023-06-08 18:13:21 +0800

    added navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import NavbarBM from './navbar';
 
 function App() {
   return (
     <div className="App">
+      <NavbarBM />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>Hello from my testing app, M5ddie!</h1>
